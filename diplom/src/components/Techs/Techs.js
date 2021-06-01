acedf8ad61306f566c1d735b7c33ddc1ac9bc484
Lamara Lanchava <58632996+lamchik@users.noreply.github.com>
2021-06-01 19:39:16 +0300

    адаптив главной страницы готов

--- diplom/src/components/Techs/Techs.js
+++ diplom/src/components/Techs/Techs.js
@@ -8,33 +8,36 @@ function Tech() {
         <div className="tech">
             <h3 className="tech__header main-header"><a name="tech"></a>Технологии</h3>
             <hr className="tech__line main-line"></hr>
-            <div className="tech__description-wrap">
-                <h2 className="tech__count">7 технологий</h2>
-                <p className="tech__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+            <div className="tech__description-name-wrap">
+                <div className="tech__description-wrap">
+                    <h2 className="tech__count">7 технологий</h2>
+                    <p className="tech__description">На курсе веб-разработки мы освоили технологии, которые применили в дипломном проекте.</p>
+                </div>
+                <div className="tech__name-wrap">
+                    <div className="tech__name">
+                        <p className="tech__name-text">HTML</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">CSS</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">JS</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">React</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">Git</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">Express.js</p>
+                    </div>
+                    <div className="tech__name">
+                        <p className="tech__name-text">mongoDB</p>
+                    </div>
+                </div>
             </div>
-            <div className="tech__name-wrap">
-                <div className="tech__name">
-                    <p className="tech__name-text">HTML</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">CSS</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">JS</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">React</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">Git</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">Express.js</p>
-                </div>
-                <div className="tech__name">
-                    <p className="tech__name-text">mongoDB</p>
-                </div>
-            </div>
+            
            
         </div>
         
